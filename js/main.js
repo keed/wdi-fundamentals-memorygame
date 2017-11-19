4f8f85c6435109f3e0d5a23ef5db0966c149e416
Darren Koh <darren212@gmail.com>
2017-11-20 07:33:05 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -5,7 +5,29 @@ var cardThree = "king";
 var cardFour = "queen";
 */
 
-var cards = ['queen', 'queen', 'king', 'king'];
+//var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 
@@ -20,10 +42,13 @@ function checkForMatch() {
 	}
 }
 
-function flipCard (cardId) {
-	var cardOne;
+function flipCard() {
 
+	var cardId = this.getAttribute("data-id");
+	//var cardId = document.getElementById("game-board").getAttribute("data-id");
+	console.log("cardId = "+ cardId);
 	/*
+	var cardOne;
 	cardOne = cards[0];
 	cardsInPlay.push(cardOne);
 
@@ -35,10 +60,12 @@ function flipCard (cardId) {
 	console.log("User flipped "+ cardTwo);
 	*/
 
-	console.log("User flipped "+cards[cardId]);
+	console.log("User flipped "+cards[cardId].rank);
+	console.log("User flipped "+cards[cardId].suit);
+	console.log("User flipped "+cards[cardId].cardImage);
 
-	cardsInPlay.push(cards[cardId]);
-
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 
 	/*
@@ -53,13 +80,22 @@ function flipCard (cardId) {
 	*/
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
 
-
-/*
-for (var i = 0; i < cards.length; i++) {
-	
+		//Adding Event Listener
+		cardElement.addEventListener("click", flipCard);
+		//cardElement.addEventListener("click", function() { alert();});
+		//Append
+		document.getElementById("game-board").appendChild(cardElement);
+	}
 }
-*/
+
+//flipCard(0);
+//flipCard(2);
+
+createBoard();
 
