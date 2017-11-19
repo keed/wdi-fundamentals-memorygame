d1a52ee7110572e91228c6edd39eb0c915b419f4
Darren Koh <darren212@gmail.com>
2017-11-20 04:48:56 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
+/*
 var cardOne = "king";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "queen";
+*/
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+var cardOne;
+
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
 
 console.log("User flipped "+ cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped "+ cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] == cardsInPlay[1]) {
+		alert ("You found a match!");
+	}
+	else {
+		alert ("Sorry, try again");	
+	}
+}
+
+/*
+for (var i = 0; i < cards.length; i++) {
+	
+}
+*/
+
